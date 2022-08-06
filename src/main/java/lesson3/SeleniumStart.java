36d8d31ef85835d0528ced972cc0b4ca9ea26722
Merroks <89959496+Merroks@users.noreply.github.com>
2022-08-06 16:07:39 +1000

    1

---
+++ src/main/java/lesson3/SeleniumStart.java
@@ -0,0 +1,9 @@
+package lesson3;
+
+public class SeleniumStart {
+    public static void main (String[] args){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+
+    }
+}
