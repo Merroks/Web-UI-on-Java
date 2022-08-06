cb36754a0cd764babf32e50d37bf2da24c920e00
Merroks <89959496+Merroks@users.noreply.github.com>
2022-08-06 18:24:17 +1000

    2

--- src/main/java/lesson3/SeleniumStart.java
+++ src/main/java/lesson3/SeleniumStart.java
@@ -1,9 +1,13 @@
 package lesson3;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumStart {
     public static void main (String[] args){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 
-
+        WebDriver driver = new ChromeDriver();
+        driver.get ("https://google.com");
     }
 }
